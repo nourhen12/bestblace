eb366a369a4abd90d53935831cc4d06dec78ebfe
Nourhen Lahmer <noorhen.lh@gmail.com>
2021-11-27 18:18:38 +0100

    geoloctor1

--- lib/Screens/google_map/map.dart
+++ lib/Screens/google_map/map.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutterbestplace/constants.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
 
 class MapSample extends StatefulWidget {
   @override
@@ -13,22 +15,69 @@ class MapSampleState extends State<MapSample> {
   var _controller = Completer();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(35.504987, 11.043340),
     zoom: 14.4746,
   );
+
+  Future getPostion() async {
+    bool services;
+    LocationPermission per;
+    services = await Geolocator.isLocationServiceEnabled();
+    if (services == false) {
+      AwesomeDialog(
+        context: context,
+        title: 'services',
+        body: Text('Activate the localisation in your smartphone'),
+      )..show();
+      if (per == LocationPermission.denied) {
+        per = await Geolocator.requestPermission();
+      }
+    }
+
+    per = await Geolocator.checkPermission();
+    print(services);
+    print('///////////////////// per : $per');
+  }
+
+  @override
+  void initState() {
+    getPostion();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      body: ElevatedButton(
+        child: Text(
+          "button",
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+            primary: kPrimaryColor,
+            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+            textStyle: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+                fontWeight: FontWeight.w100)),
+      ),
+    );
+  }
+
+/*
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (controller) {
           _controller.complete(controller);
         },
       ),
     );
-  }
+  }*/
 }
 
 //KeyAPI android : AIzaSyAp6_rBkKNIbEe0QLj5rn5IuBYV_EpoJr4
